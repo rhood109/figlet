67c24c41d31f99184cd9416efec77db13ace5b17
rhood109 <113286204+rhood109@users.noreply.github.com>
2022-11-04 14:35:47 +0000

    Add files via upload

---
+++ tool/ip_addreas.py
@@ -0,0 +1,186 @@
+import os
+import socket
+import time
+import sys
+#os.system("clear")
+print ()
+#$$$$$$$$$$$$$$$$$$$$$$$#
+#############################222222222222222##########################
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.01)
+ ###############################################################
+class main:
+    def name():
+        print (' ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ \033[1;93m1%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ \033[1;93m5%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ \033[1;93m10%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ \033[1;93m15%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ \033[1;93m25%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ \033[1;93m30%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ \033[1;93m40%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ \033[1;93m50%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ \033[1;93m60%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m65%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m70%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m75%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m80%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m85%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m 90%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ ▇ █ █ ▇ █ █ █ █ █ █ █ █ █ \033[1;93m 95%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▂ ▃ ▄ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m 99%")
+        time.sleep(0.2)
+        print ('            ')
+        print ("\033[92mDone.....!")
+
+#$$$$$$$$$$$$$$$$$$$$$$$#
+
+
+
+#$$$$$$$$$$$$$$$$$$$$$$#
+        def t(nn,t_):
+                print(t_)
+                print('')                                                                                            
+                for i in range(0,33):
+                    i+=1
+                    txt='\033[1;32m▒'*43
+                    f=i*'\033[1;31m▊'
+                    tt=i*3+1
+                    ttt=str(tt)
+                    print(txt+'┊'+    ttt+'%',end='\r')
+                    print('Lime Arch┊{}'.format(f),end='\r')
+                    time.sleep(nn)
+                print('')
+
+
+        t(0.1,' \n\t   \033[1;31m [   PLEASE WAIT .... ]')
+        print ('\033[1;33mDone............')
+        print (' ')
+        t(0.1,'\n\t   \033[1;31m[ TESTING YOUR DEVICE .... ]')
+        print ('\033[1;33mDone.................')
+        print (' ')
+        t(0.1,'\n\t   \033[1;31m[ TESTING YOUR CPU .... ]')
+        print ('\033[1;33mDone.....................')
+        print (' ')
+        t(0.1,'\n\t   \033[1;31m[ FIX & REPAIRE YOUR SYSTEM .... ]')
+        print (' ')
+        time.sleep(0.2)
+        print ('\033[1;33mDone............')
+        print (' ')
+
+#$$$$$$$$$$$$$$$$$$$$$$$#
+        time.sleep(5)
+        os.system("clear")
+        time.sleep(2)
+        print ("\033[1;33m        \    \    \. \\\.__,-“-.__.///  ./  ./    /  ")
+        time.sleep(0.2)
+        print ("\033[1;33m         \.   \`.  \`.-'“ _,=|=._ “”`-.’/  ’/   ./  ")
+        time.sleep(0.2)
+        print ("\033[1;33m          \`.  \_`-’’ //  _,=“=._  \\  ``-_/   ./   ")
+        time.sleep(0.2)
+        print ("\033[1;33m           \`-’,\=,'\\_. \ _,=“=._//,_///_./,`-'/     ")
+        time.sleep(0.2)
+        print ("\033[1;33m            \.   \.      __,-“-.__   \033[1;31m   ./   ./       ")
+        time.sleep(0.2)
+        print ("\033[1;31m        \.  \033[1;33m \`. \ `.-'“” _,=“=._ “”`-.\033[1;31m’/  .’/   ./    ")
+        time.sleep(0.2)
+        print ("\033[1;31m         \`. \033[1;31m \_`.““      _,=“=._      ``-'\033[1;31m_/  .’/      ")
+        time.sleep(0.2)
+        print ("\033[1;31m          \ `-\033[1;31m',-._   _.  _,=“=._  ,_   _.-,`-’\033[1;31m /      ")
+        time.sleep(0.2)
+        print ("\033[1;31m       \. /`,-'\033[1;33m,-._\033[1;31m”””  \ _,=“=._ /  “””\033[1;33m_.-,\033[1;31m`-,’\ ./  ")
+        time.sleep(0.2)
+        print ("\033[1;31m        \`-' \033[1;33m /    `-._ \033[1;31m “       “ \033[1;33m _.-’    \ \033[1;31m `-’/  ")
+        time.sleep(0.2)
+        print ("\033[1;31m        /)  \033[1;33m (   \033[1;31m @ \033[1;33m   \ \033[1;31m   ,-.   \033[1;33m /   \033[1;31m @ \033[1;33m   ") 
+        time.sleep(0.2)
+        print ("\033[1;31m     ,-’    \033[1;33m `-.       \ \033[1;31m /   \ \033[1;33m /       .-'   \033[1;31m  ”`-, ")
+        time.sleep(0.2)
+        print ("\033[1;31m   ,’_._      \033[1;33m   `-.____/ \033[1;31m/  _  \ \033[1;33m\____.-'     \033[1;31m    _._`,  ")
+        time.sleep(0.2)
+        print ("\033[1;31m  /,’   `.               \033[1;31m \_/ \_/             \033[1;31m   .'   `,\   ")
+        time.sleep(0.2)
+        print ("\033[1;31m /'       )                  _                  (       `\    ")
+        time.sleep(0.2)
+        print ("\033[1;31m         /   _,-’“`-.  ,++|T|||T|++.  .-'“`-,_   \            ")
+        time.sleep(0.2)
+        print ("\033[1;31m        / ,-’        \/|`|`|`|'|’|‘|\/        `-, \          ")
+        time.sleep(0.2)
+        print ("\033[1;31m       /,'            || | | | | | ||            `,\        ")
+        time.sleep(0.2)
+        print ("\033[1;31m      /'               ` | | | | | ’               `\      ")
+        time.sleep(0.2)
+        print ("\033[1;31m                         ` | | | '                        ")
+        time.sleep(0.2)
+        print (' ')
+
+#$$$$$$$$$$$$$$$$$$$$$$$#
+        time.sleep(2)
+        hostname =input("\033[1;32mEnter Your DNS Or Target: \033[1;31m")
+
+        ip=socket.gethostbyname(hostname)
+#$$$$$$$$$$$$$$$$$$$$$$#
+        os.system('clear')
+        time.sleep(1)
+        slowprint  ("\033[1;35m ____       _                  _   _           _")
+        slowprint ("\033[1;31m|  _ \ ___ | |__   ___ _ __   | | | | ___   __| | ___")
+        slowprint ("\033[1;32m| |_) / _ \| '_ \ / _ \ '_ \  | |_| |/ _ \ / _` |/ _ \ ")
+        slowprint ("\033[1;33m|  _ < (_) | |_) |  __/ | | | |  _  | (_) | (_| |  __/")
+        slowprint ("\033[1;34m|_| \_\___/|_.__/ \___|_| |_| |_| |_|\___/ \__,_|\___|")
+
+#$$$$$$$$$$$$$$$$$$$$$$$#
+        time.sleep(0.2)
+        print ("\033[1;35mHost Name Is: \033[1;34m",hostname)
+        time.sleep(0.2)
+        print ('\033[1;35mTarget Ip Is:\033[1;33m ',ip)
+        time.sleep(0.2)
+        print ("\033[1;32mdone ...")
+    name()
+
+
+
+
+
+
+
+
+
+
+
+
+
